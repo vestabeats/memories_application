81dbfbb81cbed05b7cbd48f2d0b5666216f6fff6
vestabeats <vestabeats12@gmail.com>
2024-03-09 04:55:22 +0100

    update

--- server/index.js
+++ server/index.js
@@ -14,12 +14,7 @@ dotenv.config();
 // Enable CORS
 app.use(cors());
 
-// Create a directory for file uploads
-const uploadDirectory = process.cwd() + '/uploads';
-if (!fs.existsSync(uploadDirectory)) {
-  fs.mkdirSync(uploadDirectory);
-  console.log('Upload directory created:', uploadDirectory);
-}
+
 
 // Set up body parser with increased payload size limit
 app.use(bodyParser.json({ limit: '100mb', extended: true }));
@@ -29,22 +24,7 @@ app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-// Configure multer storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDirectory);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
 
-const upload = multer({ storage: storage, limits: { fileSize: '100mb' } });
-
-// Handle file upload using multer middleware
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.status(200).json({ message: 'File uploaded and stored' });
-});
 
 // Connect to MongoDB and start the server
 const PORT = process.env.PORT || 5000;
